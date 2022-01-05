6f101e819f0c3dd5716f139b223d7877a0550831
aLonelyPixel <dlmlzz50@gmail.com>
2022-01-05 13:15:25 +0100

    L5 done

---
+++ L5/dnsAddressesSetup.ps1
@@ -0,0 +1,12 @@
+﻿$min = 150;
+$max = 153;
+$serverName = ""
+$domainName = "dalmolin.local"
+$entryName = "ip-192-168-190-"
+$ip = "192.168.190."
+$inverseIp = "190.168.192.in-addr.arpa"
+
+for($i=$min; $i -lt $max; $i++) {
+    # Ajout dans la zone directe
+    $resultat = &"dnscmd" "$serverName" "/RecordAdd" "$domainName" "$entryName$i" "A" "192.168.190.$i"    # Ajout dans la zone inverse    $resultat = &"dnscmd" "$serverName" "/RecordAdd" "$inverseIp" "$i" "PTR" "$entryName$i.$domainName"
+}
